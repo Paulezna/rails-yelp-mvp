2c90f2c033c8c0dae77811859914992339a66383
Paula Gonzalez Navarro <paulezseo@gmail.com>
2023-05-31 14:59:16 +0100

    first part solved

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  validates :name, :address, :phone_number, presence: true
+  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  has_many :reviews, dependent: :destroy
+end
---
+++ db/migrate/20230531135818_rename_phone_to_phone_number_in_restaurants.rb
@@ -0,0 +1,5 @@
+class RenamePhoneToPhoneNumberInRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :restaurants, :phone, :phone_number
+  end
+end
